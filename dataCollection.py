62da79b3bad250e3fa50c232e89f85733e7fb908
User <user@UserdeMacBook-Pro.local>
2021-02-27 19:32:22 -0600

    Add docstring for dataCollection class

--- dataCollection.py
+++ dataCollection.py
@@ -18,64 +18,65 @@ class DataCollection():
         # self.collection.create_index("book_id", unique=True)
 
     def push_to_collection(self, book_data):
-        """[summary]
+        """Add an entry to database
 
         Args:
-            book_data ([type]): [description]
+            book_data (dictionary): information of a book
         """
         if 'url' in book_data:
             if not self.url_already_exist(book_data['url']):
                 self.collection.insert_one(book_data)
 
     def get_collection(self):
-        """[summary]
+        """Get collection name of DataCollection
 
         Returns:
-            [type]: [description]
+            str: Collection name
         """
         return self.collection
 
     def empty_data_collection(self):
-        """[summary]
+        """Empty a data collection in the remote database
         """
         self.collection.delete_many({})
         # self.create_collection()
 
     def url_already_exist(self, url):
-        """[summary]
+        """Check if an url of a book/author is already in the database
 
         Args:
-            url ([type]): [description]
+            url (str): url
 
         Returns:
-            [type]: [description]
+            Boolean: True if already exist, false otherwise
         """
         query = {"url": url}
         return self.collection.count_documents(query) != 0
 
     def get_all_entries(self):
-        """[summary]
+        """Get all data in the database
 
         Returns:
-            [type]: [description]
+            [Dictionary]: A list of dictionaries containing all information
+                          in the database
         """
         return self.collection.find()
 
     def document_already_exist(self, document):
-        """[summary]
+        """Check if an entry is already in the database
 
         Args:
-            document ([type]): [description]
+            document (dictionary): A dictionary containing information of a book/author
 
         Returns:
-            [type]: [description]
+            Boolean: True if document already exist, false otherwise
         """
         return self.collection.count_documents(document)
 
     def get_collection_size(self):
-        """[summary]
+        """Get the number of documents in the database
 
         Returns:
-            [type]: [description]
+            int : The size of database
         """
         return self.collection.count_documents({})
