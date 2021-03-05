3c7bf95771e33976027cd64acf97cf1516e3899c
User <user@UserdeMacBook-Pro.local>
2021-03-04 22:14:55 -0600

    Implement author GET

--- src/api.py
+++ src/api.py
@@ -16,24 +16,31 @@ author_data_collection = DataCollection(MONGO_CONNECTION_STRING, "goodReads", 'a
 def list_to_dict(input_list):
     return {"data":input_list}
 
+def get_data(args, data_collection):
+    if(len(args) == 0):  # no field params passed in, return all data
+        all_data = list(data_collection.get_all_entries())
+        for entry in all_data:
+            entry['_id'] = str(entry['_id'])
+        return list_to_dict(all_data)
+    else:
+        if args['id']:
+            data_info = data_collection.find_by_id(args['id'])
+            if(data_info is None):
+                abort(404)
+            data_info['_id'] = str(data_info['_id'])
+            return list_to_dict(data_info)
+        abort(400)
+
 # api/book?id={attr_value} Example: /book?id=3735293
 @app.route('/api/book', methods=['GET'])
 def get_book():
     args = request.args
-    if(len(args) == 0):  # no field params passed in, return all book data
-        books = list(book_data_collection.get_all_entries())
-        for book in books:
-            book['_id'] = str(book['_id'])
-        return list_to_dict(books)
-    else:
-        if args['id']:
-            book_info = book_data_collection.find_by_id(args['id'])
-            if(book_info is None):
-                abort(404)
-            book_info['_id'] = str(book_info['_id'])
-            return book_info
-    return
-    # return args['id']
+    return get_data(args, book_data_collection)
+
+@app.route('/api/author', methods=['GET'])
+def get_author():
+    args = request.args
+    return get_data(args, author_data_collection)
 
 
 
